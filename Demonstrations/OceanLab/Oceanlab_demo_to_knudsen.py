dbd1af180c939f5adc4c10697ab2fb12a5426e6a
TreesaJ <treesarjc@gmail.com>
2024-07-11 13:57:58 +0200

    Oceanlab demo

---
+++ Demonstrations/OceanLab/Oceanlab_demo_to_knudsen.py
@@ -0,0 +1,242 @@
+# Import necessary libraries
+import json
+
+from otelib import OTEClient
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+url = os.getenv("URL")
+USER = os.getenv("USER")
+PASSWORD = os.getenv("PASSWORD")
+DATABASE = os.getenv("DATABASE")
+RETPOLICY = os.getenv("RETPOLICY")
+
+try:
+    # Initialize the OTEAPI client with the API's base URL.
+    client = OTEClient("http://localhost:8080")
+
+    # Display the client configuration for verification.
+    print("OTEAPI Client Configuration:", client)
+
+    parser = client.create_parser(
+        entity="http://onto-ns.com/meta/oceanlab/1/ctd_data_munkholmen",
+        parserType="influx/vnd.dlite-influx",
+        configuration={
+            "storage_path": "/entities",
+            "url": url,
+            "USER": USER,
+            "PASSWORD": PASSWORD,
+            "DATABASE": DATABASE,
+            "RETPOLICY": RETPOLICY,
+        },
+    )
+    # Display the ID of the created data resource strategy for tracking.
+    print("Parser Strategy ID:", parser.strategy_id)
+
+    # Build the data pipeline by chaining the data resource to the parser.
+    pipeline = parser
+
+    # Execute the pipeline to fetch and parse the data, then decode the result from bytes to a string.
+    result = pipeline.get().decode("utf-8")
+
+    # Load the string result into a JSON object for further manipulation or analysis.
+    json_object = json.loads(result)
+
+    # Pretty-print the JSON object to make it easier to read.
+    json_formatted_str = json.dumps(json_object, indent=4)
+    print("Formatted JSON Result:\n", json_formatted_str)
+
+except Exception as e:
+    print("An error occurred:", e)
+
+try:
+    dataMappings = [
+        (
+            "http://onto-ns.com/meta/oceanlab/1/ctd_data_munkholmen#salinity",
+            "http://emmo.info/domain-mappings#mapsTo",
+            "http://www.semanticweb.org/ocean_data/cf_standards/Oceanlab/0.0.1#sea_water_practical_salinity",
+        ),
+        (
+            "http://www.semanticweb.org/ocean_data/cf_standards/Oceanlab/0.0.1#sea_water_practical_salinity",
+            "http://emmo.info/domain-mappings#mapsTo",
+            "http://fun.info#v",
+        ),
+        (
+            "http://onto-ns.com/meta/oceanlab/1/ctd_data_munkholmen#pressure",
+            "http://emmo.info/domain-mappings#mapsTo",
+            "http://www.semanticweb.org/ocean_data/cf_standards/Oceanlab/0.0.1#sea_water_pressure",
+        ),
+        (
+            "http://onto-ns.com/meta/oceanlab/1/ctd_data_munkholmen#conductivity",
+            "http://emmo.info/domain-mappings#mapsTo",
+            "http://www.semanticweb.org/ocean_data/cf_standards/Oceanlab/0.0.1#sea_water_electrical_conductivity",
+        ),
+        (
+            "http://onto-ns.com/meta/oceanlab/1/ctd_data_munkholmen#density",
+            "http://emmo.info/domain-mappings#mapsTo",
+            "http://www.semanticweb.org/ocean_data/cf_standards/Oceanlab/0.0.1#sea_water_density",
+        ),
+        (
+            "http://onto-ns.com/meta/oceanlab/1/ctd_data_munkholmen#time",
+            "http://emmo.info/domain-mappings#mapsTo",
+            "http://www.wikidata.org/entity/Q186885",
+        ),
+        
+      
+      
+    ]
+    mapping = client.create_mapping(mappingType="mappings", triples=dataMappings)
+    print(mapping.strategy_id)
+except Exception as e:
+    print(f"Error creating mapping_for_results mapping: {e}")
+
+try:
+    dataMappings_salinity = [
+        (
+            "http://onto-ns.com/meta/oceanlab/1/ctd_salinity_munkholmen#salinity",
+            "http://emmo.info/domain-mappings#mapsTo",
+            "http://hall_petch.info/ToKnudsen",
+        ),
+        (
+            "http://hall_petch.info/ToKnudsen",
+            "http://emmo.info/domain-mappings#mapsTo",
+            "http://fun.info#ret",
+        ),
+        (
+            "http://onto-ns.com/meta/oceanlab/1/ctd_salinity_munkholmen#time",
+            "http://emmo.info/domain-mappings#mapsTo",
+            "http://www.wikidata.org/entity/Q186885",
+        ),
+    ]
+    mapping_salinity = client.create_mapping(
+        mappingType="mappings", triples=dataMappings_salinity
+    )
+    print(mapping_salinity.strategy_id)
+except Exception as e:
+    print(f"Error creating mapping_salinity: {e}")
+
+# function mappings --> but eventually this be extracted from a triplestore
+try:
+    function_ontology_mappings = [
+       (
+            "http://onto-ns.com/meta/0.4/HallPetch2#ToKnudsen",
+            "http://www.w3.org/1999/02/22-rdf-syntax-ns#type",
+            "https://w3id.org/function/ontology#Function",
+        ),
+        (
+            "http://onto-ns.com/meta/0.4/HallPetch2#ToKnudsen",
+            "http://www.w3.org/2000/01/rdf-schema#label",
+            '"ToKnudsen"@en',
+        ),
+        (
+            "http://onto-ns.com/meta/0.4/HallPetch2#ToKnudsen",
+            "https://w3id.org/function/ontology#expects",
+            "_:http://onto-ns.com/meta/0.4/HallPetch2#ToKnudsen_parlist",
+        ),
+        (
+            "http://onto-ns.com/meta/0.4/HallPetch2#ToKnudsen",
+            "https://w3id.org/function/ontology#returns",
+            "_:http://onto-ns.com/meta/0.4/HallPetch2#ToKnudsen_outlist",
+        ),
+        (
+            "http://onto-ns.com/meta/0.4/HallPetch2#ToKnudsen_parameter1_v",
+            "http://www.w3.org/1999/02/22-rdf-syntax-ns#type",
+            "https://w3id.org/function/ontology#Parameter",
+        ),
+        (
+            "http://onto-ns.com/meta/0.4/HallPetch2#ToKnudsen_parameter1_v",
+            "http://www.w3.org/2000/01/rdf-schema#label",
+            '"v"@en',
+        ),
+        (
+            "http://onto-ns.com/meta/0.4/HallPetch2#ToKnudsen_parameter1_v",
+            "http://emmo.info/domain-mappings#mapsTo",
+            "http://fun.info#v",
+        ),
+        (
+            "_:http://onto-ns.com/meta/0.4/HallPetch2#ToKnudsen_parlist",
+            "http://www.w3.org/1999/02/22-rdf-syntax-ns#first",
+            "http://onto-ns.com/meta/0.4/HallPetch2#ToKnudsen_parameter1_v",
+        ),
+        (
+            "_:http://onto-ns.com/meta/0.4/HallPetch2#ToKnudsen_parlist",
+            "http://www.w3.org/1999/02/22-rdf-syntax-ns#rest",
+            "http://www.w3.org/1999/02/22-rdf-syntax-ns#nil",
+        ),
+        (
+            "http://onto-ns.com/meta/0.4/HallPetch2#ToKnudsen_output1",
+            "http://www.w3.org/1999/02/22-rdf-syntax-ns#type",
+            "https://w3id.org/function/ontology#Output",
+        ),
+        (
+            "http://onto-ns.com/meta/0.4/HallPetch2#ToKnudsen_output1",
+            "http://emmo.info/domain-mappings#mapsTo",
+            "http://fun.info#ret",
+        ),
+        (
+            "_:http://onto-ns.com/meta/0.4/HallPetch2#ToKnudsen_outlist",
+            "http://www.w3.org/1999/02/22-rdf-syntax-ns#first",
+            "http://onto-ns.com/meta/0.4/HallPetch2#ToKnudsen_output1",
+        ),
+        (
+            "_:http://onto-ns.com/meta/0.4/HallPetch2#ToKnudsen_outlist",
+            "http://www.w3.org/1999/02/22-rdf-syntax-ns#rest",
+            "http://www.w3.org/1999/02/22-rdf-syntax-ns#nil",
+        ),
+        (
+            "http://onto-ns.com/meta/0.4/HallPetch2#ToKnudsen",
+            "http://emmo.info/oteio#hasPythonFunctionName",
+            "to_knudsen",
+        ),
+        (
+            "http://onto-ns.com/meta/0.4/HallPetch2#ToKnudsen",
+            "http://emmo.info/oteio#hasPythonModuleName",
+            "src.mymath.util",
+        ),
+        (
+            "http://onto-ns.com/meta/0.4/HallPetch2#ToKnudsen",
+            "http://emmo.info/oteio#hasPypiPackageName",
+            "git+https://github.com/Treesarj/converters",
+        ),
+    ]
+    function_mappings = client.create_mapping(
+        mappingType="mappings", triples=function_ontology_mappings
+    )
+    print(function_mappings.strategy_id)
+except Exception as e:
+    print(f"Error creating second mapping: {e}")
+    
+    
+# Create a function to generate output based on the specified configuration.
+try:
+    generate = client.create_function(
+        functionType="application/vnd.dlite-generate",
+        configuration={
+            "driver": "json",
+            "location": "/output/hp.json",
+            "datamodel": "http://onto-ns.com/meta/oceanlab/1/ctd_salinity_munkholmen",
+        },
+    )
+    print(generate.strategy_id)
+except Exception as e:
+    print(f"Error creating generate function: {e}")
+
+
+# Build the data pipeline by chaining together the data resource, parser, mappings, and generate function.
+try:
+    pipeline = parser >> mapping >> mapping_salinity >> function_mappings >> generate
+
+    # Execute the pipeline and process the data.
+    result = pipeline.get().decode("utf-8")
+    print(result)
+    # Convert the result into a JSON object and then format it for readability.
+    json_object = json.loads(result)
+    json_formatted = json.dumps(json_object, indent=4)
+    print(json_formatted)
+
+    # Inform the user that the data has been processed and output is written.
+    print("Data is written out to output/hp.json")
+except Exception as e:
+    print(f"Error executing pipeline: {e}")
---
+++ Demonstrations/OceanLab/README.md
@@ -0,0 +1,30 @@
+## Step 1: Run the Demos
+
+To run the demo, ensure your virtual environment is activated and environment variables is set.
+Then execute:
+
+### Demo1
+
+This extracts all the data from the influxdb
+
+To run the demo:
+`bash
+	python Oceanlab_demo_extract_same.py
+	`
+
+### Demo2
+
+This extracts just the salinity from the influxdb 
+
+To run the demo:
+`bash
+	python Oceanlab_demo_get_salinity.py
+	`
+### Demo3
+
+This extracts the salinity and transforms it to knudsen salinity 
+
+To run the demo:
+`bash
+	python Oceanlab_demo_to_knudsen.py
+	`
